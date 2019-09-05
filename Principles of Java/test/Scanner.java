01f8693260bedd2962b32f48498b013aa24a6582
SN1CK3T <47250557+SN1CK3T@users.noreply.github.com>
2019-09-04 21:28:14 -0400

    added a test folder

---
+++ Principles of Java/test/Scanner.java
@@ -0,0 +1,10 @@
+//Ben McDaniel
+
+public class Scanner{
+    public static void main(String[]args){
+    Scanner number = Scanner(System.in);
+    System.out.print("Enter a Number:");
+    number = scan.nextInt();
+    System.out.println(number);
+}
+}
