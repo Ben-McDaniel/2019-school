148bfa2b5ed40711d439758186f353bbccde2445
Ben M <benjamin.mcdaniel123@gmail.com>
2019-09-05 18:01:30 -0400

    09.05.19 pt2

--- Comp Sci A/FloatingPoint.java
+++ Comp Sci A/FloatingPoint.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 public class FloatingPoint{
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        int one;
-        int two;
-        int sum;
-        int dif;
-        int prd;
+        double one;
+        double two;
+        double sum;
+        double dif;
+        double prd;
 
         System.out.println("Enter number 1: "); 
         one = input.nextDouble(); 
         System.out.println("Enter number 2: ");
         two = input.nextDouble();
+
+
+        sum = one + two;
+        dif = one - two;
+        prd = one * two;
+
+        System.out.println("The Sum is: " + sum);
+        System.out.println("The Difference is: " + dif);
+        System.out.println("The Product is: " + prd);
     }
 }
