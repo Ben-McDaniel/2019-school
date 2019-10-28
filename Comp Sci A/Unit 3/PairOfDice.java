221e3626e459bd47f266a64682f6430ad1e6d5c1
SN1CK3T <47250557+SN1CK3T@users.noreply.github.com>
2019-10-27 23:51:13 -0400

    10.27.19 3

--- Comp Sci A/Unit 3/PairOfDice.java
+++ Comp Sci A/Unit 3/PairOfDice.java
@@ -1,20 +1,20 @@
 //Ben McDaniel
 
+import java.lang.*;
+import java.util.*;
+
 public class PairOfDice{
     Die d6a = new Die();
     Die d6b = new Die();
 
     private int check1, check2;
 
-    public boolean roll(){
+    public int roll(){
         check1 = d6a.roll(6);
         check2 = d6b.roll(6);
 
-        if (check1 == 6 && check2 == 6){
-            return(true);
-        }else{
-            return(false);
-        }
+        return(check1 + check2);
+        
     }
 
 
---
+++ Comp Sci A/Unit 3/Pig.java
@@ -0,0 +1,11 @@
+//Ben McDaniel
+
+public class Pig{
+    public static void main(String[]args){
+        PairOfDice cpu = new PairOfDice();
+        PairOfDice player = new PairOfDice();
+
+        System.out.println(cpu.roll());
+        
+    }
+}
