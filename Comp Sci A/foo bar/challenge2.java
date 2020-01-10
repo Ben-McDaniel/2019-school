af5e84f1c9f9a313f614d4281ed79c662dee6421
Ben M <benjamin.mcdaniel123@gmail.com>
2020-01-10 09:09:45 -0500

    broke shit

--- Comp Sci A/foo bar/challenge2.java
+++ Comp Sci A/foo bar/challenge2.java
@@ -10,19 +10,31 @@ public class challenge2 {
     public static int[] solution(int[] pegs) {
         // Your code here
         int [] noSolution = {-1, -1};
-        int maxSize, finalGear = 0;
+        int maxSize, size, finalGear = 0;
 
         maxSize = pegs[1] - pegs[0] -1;
+
         for (int i = 1; i <= maxSize; i++){
             //go through whole array figuring out what size the final gear will be
+            size = i;
             for (int j = 1; j < pegs.length; j++){
-                finalGear = pegs[j] - pegs[j-1] - i - j;
+                size = nextGearSize(pegs, j, size);
+                finalGear = size;
             }
+            finalGear = (finalGear - 1) / 2;
+            System.out.println(finalGear);
+
             if (i / finalGear == 2){
                 int[] solution = {i, 1};
                 return solution;
             }
         }
         return noSolution;
+    }
+
+    public static int nextGearSize(int[] pegs, int pos, int previousSize){
+        int size = pegs[pos] - pegs[pos - 1] - 1;
+        System.out.println(size);
+        return size;
     }
 }
