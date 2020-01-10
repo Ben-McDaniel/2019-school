4362593b4718fb7b0d0e9507ab88017d2cbbbf50
SN1CK3T <47250557+SN1CK3T@users.noreply.github.com>
2020-01-09 20:10:06 -0500

    01.09.20 4

--- Comp Sci A/foo bar/challenge2.java
+++ Comp Sci A/foo bar/challenge2.java
@@ -6,21 +6,23 @@ public class challenge2 {
         System.out.println(Arrays.toString(solution(pegs)));
     }
 
+    
     public static int[] solution(int[] pegs) {
         // Your code here
         int [] noSolution = {-1, -1};
-        int maxSize, minSize, finalGear;
+        int maxSize, finalGear = 0;
 
         maxSize = pegs[1] - pegs[0] -1;
-        System.out.println(maxSize);
-
-
-
-
-
-
-
-
+        for (int i = 1; i <= maxSize; i++){
+            //go through whole array figuring out what size the final gear will be
+            for (int j = 1; j < pegs.length; j++){
+                finalGear = pegs[j] - pegs[j-1] - i - j;
+            }
+            if (i / finalGear == 2){
+                int[] solution = {i, 1};
+                return solution;
+            }
+        }
         return noSolution;
     }
 }
